6b7c46598763869484123e3b7714ae9aca74dae2
prakashmk <30828492+prakashmk@users.noreply.github.com>
2018-02-13 15:10:39 +0530

    Create p52.c

---
+++ p52.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("Enter the number");
+scanf("%d",&a);
+switch(a){
+case 1: printf("One");
+       break;
+case 2: printf("Two");
+       break;       
+case 3: printf("Three");
+       break;       
+case 4: printf("Four");
+       break;       
+case 5: printf("five");
+       break;       
+case 6: printf("six");
+        break;
+case 7: printf("Seven");
+       break;
+case 8: printf("Eight");
+       break;
+case 9: printf("Nine");
+       break;
+case 10: printf("Ten");
+       break;   
+default: printf("No word"); 
+}
+return 0;
+}
